b8a14826e8f119c330654505d4c6e6f24105c84c
Michael Gillett <michael.gillett@outlook.com>
2025-04-08 13:53:09 +0000

    Add database authentication options for djongo in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -86,6 +86,12 @@ DATABASES = {
     }
 }
 
+# djongo settings
+DATABASES['default']['OPTIONS'] = {
+    'username': 'octofit_user',
+    'password': 'octofit_password',
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
