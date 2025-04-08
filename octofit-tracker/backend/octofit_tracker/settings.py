e6f29bd1b0d7773075e242bb90397a44d5ed0d53
Michael Gillett <michael.gillett@outlook.com>
2025-04-08 14:46:02 +0000

    Update ALLOWED_HOSTS in settings.py and modify api_root view to use a base URL

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%6-gbu9iano3ktc285j9d*x%y^i+cmc&$mg)smee(t+%oqyibs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'glowing-rotary-phone-g6prq77jw7cppx5-8000.app.github.dev']
 
 
 # Application definition
